e9e4795275be67c1896a1a0467c96f530f762b84
EJLarcs <ejlarcombe@gmail.com>
2015-10-14 11:21:15 -0400

    done

--- lib/oo_smallest_multiple.rb
+++ lib/oo_smallest_multiple.rb
@@ -1,1 +1,22 @@
 # Implement your object-oriented solution here!
+# objective: find the lowest common multiple of a range of numbers
+# how do you know its a multiple? because dividing by a number will % == 0
+# where can you cancel out? do not do ranges to numbers that aren't knowingly a multiple of the numbers you have
+class SmallestMultiple
+
+  def initialize(num)
+    @num = num
+  end
+
+  def lcm
+    counter = @num
+      loop do
+        if (1..@num).all? { |i| counter % i == 0 }
+          break
+        else
+          counter += @num
+        end
+      end
+    counter
+  end
+end
--- lib/smallest_multiple.rb
+++ lib/smallest_multiple.rb
@@ -1,1 +1,16 @@
 # Implement your procedural solution here!
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+  # Multiply each factor the greatest number of times it occurs in any of the numbers. 9 has
+  # two 3s, and 21 has one 7, so we multiply 3 two times, and 7 once. This gives us 63, the smallest
+  # number that can be divided evenly by 3, 9, and 21.
+  def smallest_multiple(num)
+    counter = num
+      loop do
+        if (1..num).all? { |i| counter % i == 0 }
+          break
+        else
+          counter += num
+        end
+      end
+    counter
+  end
